a6423e1e1d99b135d2b6c92b8099021091f733ad
tamagochka <tamagochka@gmail.com>
2018-09-27 11:30:15 +0300

    Initial commit.

---
+++ src/main/java/my/tamagochka/NIOSocketsWrapper/NIOServer.java
@@ -0,0 +1,11 @@
+package my.tamagochka.NIOSocketsWrapper;
+
+public class NIOServer extends Thread {
+
+    private static final int DEFAULT_BUFFER_SIZE = 1024;
+
+    private final int port;
+    private final int bufferSize;
+
+
+}
