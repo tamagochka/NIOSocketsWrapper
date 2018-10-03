df80adf62212032a96d617c8f7c8c21f1134b110
tamagochka <tamagochka@gmail.com>
2018-10-03 20:28:56 +0300

    Finished NIOClient constructor. Added skeleton for methods connect, write, read. Added override method run.

--- src/main/java/my/tamagochka/NIOSocketsWrapper/NIOClient.java
+++ src/main/java/my/tamagochka/NIOSocketsWrapper/NIOClient.java
@@ -1,6 +1,13 @@
 package my.tamagochka.NIOSocketsWrapper;
 
-public class NIOClient {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+public class NIOClient extends Thread {
 
     private final static int DEFAULT_BUFFER_SIZE = 1024;
 
@@ -8,18 +15,62 @@ public class NIOClient {
     private final String serverAddress;
     private final int bufferSize;
 
-    public NIOClient(String serverAddress, int serverPort) {
+    private final SocketChannel sc;
+    private final Selector selector;
+
+    public NIOClient(String serverAddress, int serverPort) throws IOException {
         this(serverAddress, serverPort, DEFAULT_BUFFER_SIZE);
     }
 
-    public NIOClient(String serverAddress, int serverPort, int bufferSize) {
+    public NIOClient(String serverAddress, int serverPort, int bufferSize) throws IOException {
         this.serverAddress = serverAddress;
         this.serverPort = serverPort;
         this.bufferSize = bufferSize;
+        sc = SocketChannel.open();
+        sc.configureBlocking(false);
+        selector = Selector.open();
+        sc.register(selector, SelectionKey.OP_CONNECT);
+        sc.connect(new InetSocketAddress(serverAddress, serverPort));
+    }
 
+    private void connect(SelectionKey key) {
+
+    }
+
+    private void read(SelectionKey key) {
+
+    }
+
+    private void write(SelectionKey key) {
+
+    }
+
+    @Override
+    public void run() {
+        while(true) { // TODO how to stop client?
+            try {
+                selector.select();
+            } catch(IOException e) {
+                continue;
+            }
+            Iterator<SelectionKey> i = selector.selectedKeys().iterator();
+            while(i.hasNext()) {
+                SelectionKey key = i.next();
+                i.remove();
+                if(!key.isValid()) continue;
+                if(key.isConnectable()) connect(key);
+                else if(key.isReadable()) read(key);
+                else if(key.isWritable()) write(key);
+            }
+
+            // TODO finishing client
+
+
+        }
 
 
 
     }
+
 
 }
